d7e6c43c19d0f62876e8bb353232899d1574c88f
Connor Johnson <35439715+TheGreatChaosCoder@users.noreply.github.com>
2022-11-05 17:25:57 -0500

    5 30 update

--- flaskmain.py
+++ flaskmain.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request
 import threading
+import sqlite3
 import time
 import random
 app = Flask(__name__)
+
+def dbConnection():
+    conn = None
+    try:
+        conn = sqlite3.connect("data.sqlite")
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 @app.route("/")
 def index():
@@ -13,14 +22,49 @@ def options():
     return render_template('options.html')
 
 @app.route('/username')
-def my_form():
+def username():
     return render_template('username.html')
 
+@app.route('/controls')
+def controls():
+    return render_template('controls.html')
+
+@app.route('/choose-names')
+def controls():
+    return render_template('choose-names.html')
+
+@app.route('/controls')
+def controls():
+    return render_template('controls.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
 @app.route('/username', methods=['POST'])
-def my_form_post():
-    text = request.form['username']
-    
-    return text.strip()
+def usernameForm():
+    try:
+        text = request.form['username'].strip()
+        msg = ""
+        with dbConnection() as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO user (username, highscore) VALUES (?,?)",(text, 0) )
+            
+            con.commit()
+            msg = "Record successfully added"
+            return render_template('info.html')
+    except:
+         con.rollback()
+         msg = "error in insert operation"
+         return render_template('info.html')
+      
+    finally:
+        cur = con.cursor()
+        cur.execute("select * from user")
+        rows = cur.fetchall()
+        #return f"<h1>{rows}</h1>"
+        return info()
+        con.close()
 
 def shutdownServer():
     func = request.environ.get('werkzeug.server.shutdown')
